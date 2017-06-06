ad79834b99dc913f8b97e3e8828d95acd0a8f958
yonh <azssjli@163.com>
2017-06-06 16:45:16 +0800

    更新注释

--- src/DirIterator.php
+++ src/DirIterator.php
@@ -2,6 +2,28 @@
 
 /**
  *
+ * 目录遍历器,通过目录遍历器遍历目录,传入文件处理类,方便实现针对遍历文件处理
+ *
+ *
+ * 一个简单的例子, 遍历当前目录,并输出所有.php的文件,首先你得有个处理类EchoPHPFileNameHandle
+ * // 调用示例
+ * $handle = new EchoPHPFileNameHandle();
+ * $iter = new DirIterator();
+ * $iter->addHandle($handle);
+ * $iter->iterator(__DIR__);
+ *
+ * class EchoPHPFileNameHandle implements DirIteratorHandle
+ * {
+ *     public function handle($spl)
+ *     {
+ *       $ext = ".php";
+ *       $pos = strpos($spl->getRealPath(), $ext);
+ *         if ($pos !== false && $pos + strlen($ext) == strlen($spl->getRealPath())) {
+ *             echo $spl->getRealPath() . PHP_EOL;
+ *         }
+ *     }
+ * }
+ *
  * User: 赖永恒
  * Date: 17-5-24
  * Time: 下午5:37
